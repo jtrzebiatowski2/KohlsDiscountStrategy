142e02721f51ac842c1197246439c5d23d73c6c4
J-Tron <J-Tron@J-Tron-PC>
2013-09-22 17:41:37 -0500

    Added a Product Class and a ProductDiscountStrategy interface Class

---
+++ src/kohlsdiscountstrategy/Product.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kohlsdiscountstrategy;
+
+/**
+ *
+ * @author J-Tron
+ */
+public class Product {
+    private int productNumber;
+    private String productDescription;
+    private double productUnitPrice;
+    
+    
+    
+    
+  
+}
---
+++ src/kohlsdiscountstrategy/ProductDiscountStrategy.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kohlsdiscountstrategy;
+
+/**
+ *
+ * @author J-Tron
+ */
+public interface ProductDiscountStrategy {
+    public abstract void getDiscount();
+}
